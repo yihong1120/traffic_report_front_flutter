01c8baec6faae7006c40aff25017983f52e91aa6
yihong1120 <qazsx748596@gmail.com>
2024-01-04 01:56:36 +0800

    Add usage of GOOGLE_MAPS_API_KEY

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,14 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // 設置 Google Maps API 密鑰
+    if let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
+      let keys = NSDictionary(contentsOfFile: path),
+      let apiKey = keys["GOOGLE_MAPS_API_KEY"] as? String {
+        GMSServices.provideAPIKey(apiKey)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
