5cfec62f662b2400994042aa0818276739e9750a
yihong1120 <qazsx748596@gmail.com>
2024-01-09 21:31:54 +0800

    Edit import of packages

--- lib/screens/accounts/login.dart
+++ lib/screens/accounts/login.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:../../services/auth_service.dart';
-
-import '../../services/auth_service.dart'; // 假設您有一個處理身份驗證的服務
+import '../../services/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
